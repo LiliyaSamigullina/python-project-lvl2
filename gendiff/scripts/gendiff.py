1a790e2d8996e6003d1ff267226aa7b42c806c7a
Liliya Samigullina <lil_1992@mail.ru>
2022-03-28 21:30:32 +0300

    update gendiff.py (step 2)

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -7,8 +7,12 @@ def main():
     parser = argparse.ArgumentParser(description='Generate diff')
     parser.add_argument('first_file')
     parser.add_argument('second_file')
+    parser.add_argument(
+        '-f', '--format',
+        help='set format of output'
+    )
     args = parser.parse_args()
-    print(args)
+    # print(args)
 
 
 if '__name__' == '__main__':

e97fd2a5be7b0f7af3212d66d25584a96803d711
Liliya Samigullina <lil_1992@mail.ru>
2022-04-04 18:16:20 +0300

    update test_generate_diff.py

--- tests/test_generate_diff.py
+++ tests/test_generate_diff.py
@@ -2,7 +2,19 @@ from gendiff import generate_diff
 
 
 with open('tests/fixtures/result_plain') as file:
-        answer = file.read()
+    plain_answer = file.read()
+
+with open('tests/fixtures/result') as file:
+    answer = file.read()
+
+
+def test_generate_diff_plain_json():
+    assert generate_diff('tests/fixtures/file1_plain.json', 'tests/fixtures/file2_plain.json') == plain_answer
+
+
+def test_generate_diff_plain_yaml():
+    assert generate_diff('tests/fixtures/file1_plain.yaml', 'tests/fixtures/file2_plain.yaml') == plain_answer
+    assert generate_diff('tests/fixtures/file1_plain.yml', 'tests/fixtures/file2_plain.yml') == plain_answer
 
 
 def test_generate_diff_json():

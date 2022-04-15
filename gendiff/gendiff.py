deeaa31abe37d401e27caa12d50dea3577ddc2f9
Liliya Samigullina <lil_1992@mail.ru>
2022-04-15 16:56:53 +0300

    update gendiff.py

--- gendiff/gendiff.py
+++ gendiff/gendiff.py
@@ -1,13 +1,13 @@
 from gendiff.comparator import get_diff_dict
-from gendiff.parser import get_dict
+from gendiff.parser import parse
 from gendiff.formatters.stylish import format_stylish
 from gendiff.formatters.plain import format_plain
 from gendiff.formatters.json import format_json
 
 
 def generate_diff(file_path1, file_path2, formatter='stylish'):
-    dict1 = get_dict(file_path1)
-    dict2 = get_dict(file_path2)
+    dict1 = parse(file_path1)
+    dict2 = parse(file_path2)
     result = get_diff_dict(dict1, dict2)
     if formatter == 'stylish':
         return format_stylish(result)

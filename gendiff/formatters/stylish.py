a74f67f07db1969ce34eb0dd4a7ed318dd06c12d
Liliya Samigullina <lil_1992@mail.ru>
2022-04-19 18:45:03 +0300

    update stylish formatter

--- gendiff/formatters/stylish.py
+++ gendiff/formatters/stylish.py
@@ -1,23 +1,23 @@
 import json
 
-TAB = '    '
+
+def get_indent(_lvl: int) -> str:
+    return '    ' * _lvl
 
 
 def format_stylish(user_dict: dict, _lvl=0) -> str:
-    indent = TAB * _lvl
-
     result = []
     prefix = {'added': '  + ', 'removed': '  - ', 'unchanged': '    '}
     for key, value in sorted(user_dict.items()):
         if value['type'] == 'changed':
             line1 = '{}{}{}: {}'.format(
-                indent,
+                get_indent(_lvl),
                 prefix['removed'],
                 key,
                 format_value(value['old_value'], _lvl + 1)
             )
             line2 = '{}{}{}: {}'.format(
-                indent,
+                get_indent(_lvl),
                 prefix['added'],
                 key,
                 format_value(value['new_value'], _lvl + 1)
@@ -25,20 +25,21 @@ def format_stylish(user_dict: dict, _lvl=0) -> str:
             line = '\n'.join([line1, line2])
         elif value['type'] == 'nested':
             line1 = '{}{}: '.format(
-                (_lvl + 1) * TAB,
+                get_indent(_lvl + 1),
                 key
             )
             line2 = format_stylish(value['value'], _lvl + 1)
             line = ''.join([line1, line2])
         else:
             line = '{}{}{}: {}'.format(
-                indent,
+                get_indent(_lvl),
                 prefix[value['type']],
                 key,
                 format_value(value['value'], _lvl + 1)
             )
         result.append(line)
-    return '{\n' + '\n'.join(result) + '\n' + indent + '}'
+    lines = '\n'.join(result)
+    return f'{{\n{lines}\n{get_indent(_lvl)}}}'
 
 
 def format_value(value, _lvl):
@@ -50,16 +51,17 @@ def format_value(value, _lvl):
         for internal_key, internal_val in value.items():
             if isinstance(internal_val, dict):
                 line = '{}{}: {}'.format(
-                    (_lvl + 1) * TAB,
+                    get_indent(_lvl + 1),
                     internal_key,
                     format_value(internal_val, _lvl + 1)
                 )
             else:
                 line = '{}{}: {}'.format(
-                    (_lvl + 1) * TAB,
+                    get_indent(_lvl + 1),
                     internal_key,
                     format_value(internal_val, _lvl)
                 )
             result.append(line)
-        return '{\n' + '\n'.join(result) + '\n' + TAB * _lvl + '}'
+        lines = '\n'.join(result)
+        return f'{{\n{lines}\n{get_indent(_lvl)}}}'
     return value

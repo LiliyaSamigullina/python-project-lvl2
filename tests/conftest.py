41c3e86c02bb724e1f941985c8221b5b2cd839ef
Liliya Samigullina <lil_1992@mail.ru>
2022-05-03 16:44:47 +0300

    fix linter errors

--- tests/conftest.py
+++ tests/conftest.py
@@ -53,7 +53,8 @@ async def result_render(request):
     assert getattr(request.module, 'FORMATTER', None)
 
     result_path = os.path.join(
-        os.path.dirname(__file__), FIXTURES_FOLDER, request.module.FORMATTER)
+        os.path.dirname(__file__), FIXTURES_FOLDER, request.module.FORMATTER
+    )
 
     with open(result_path) as file:
         return file.read()
--- tests/test_render_json.py
+++ tests/test_render_json.py
@@ -6,14 +6,26 @@ FORMATTER = 'json'
 
 @pytest.mark.asyncio
 async def test_json(file1_json_path, file2_json_path, result_render):
-    assert result_render == generate_diff(file1_json_path, file2_json_path, FORMATTER)
+    assert result_render == generate_diff(
+        file1_json_path,
+        file2_json_path,
+        FORMATTER
+    )
 
 
 @pytest.mark.asyncio
 async def test_yaml(file1_yaml_path, file2_yaml_path, result_render):
-    assert result_render == generate_diff(file1_yaml_path, file2_yaml_path, FORMATTER)
+    assert result_render == generate_diff(
+        file1_yaml_path,
+        file2_yaml_path,
+        FORMATTER
+    )
 
 
 @pytest.mark.asyncio
 async def test_yml(file1_yml_path, file2_yml_path, result_render):
-    assert result_render == generate_diff(file1_yml_path, file2_yml_path, FORMATTER)
+    assert result_render == generate_diff(
+        file1_yml_path,
+        file2_yml_path,
+        FORMATTER
+    )

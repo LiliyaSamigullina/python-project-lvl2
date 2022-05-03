1cd7ffdb7d99b33e56175dc9742b75be6c807adc
Liliya Samigullina <lil_1992@mail.ru>
2022-05-03 16:39:07 +0300

    test refactoring

---
+++ tests/test_render_plain.py
@@ -0,0 +1,19 @@
+import pytest
+from gendiff import generate_diff
+
+FORMATTER = 'plain'
+
+
+@pytest.mark.asyncio
+async def test_json(file1_json_path, file2_json_path, result_render):
+    assert result_render == generate_diff(file1_json_path, file2_json_path, FORMATTER)
+
+
+@pytest.mark.asyncio
+async def test_yaml(file1_yaml_path, file2_yaml_path, result_render):
+    assert result_render == generate_diff(file1_yaml_path, file2_yaml_path, FORMATTER)
+
+
+@pytest.mark.asyncio
+async def test_yml(file1_yml_path, file2_yml_path, result_render):
+    assert result_render == generate_diff(file1_yml_path, file2_yml_path, FORMATTER)
